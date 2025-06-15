c84d384a10eb74999442491e9945c8a2d6fe9762
kas1133 <ershov1133@gmail.com>
2025-06-15 14:52:48 +0300

    1
    1

--- src/pages/CatchEventBattle/CatchEventBattleHistory.tsx
+++ src/pages/CatchEventBattle/CatchEventBattleHistory.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {Accordion, AccordionDetails, AccordionSummary, Box, Button, Typography} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Typography } from '@mui/material';
 import { differenceInDays } from 'date-fns';
-import {getGenColor, getGenListFromIds} from '../../utils/pokemons.utils';
+import { getGenColor, getGenListFromIds } from '../../utils/pokemons.utils';
 import BattlePlayerView from './BattlePlayerView';
 import PokemonGenId from '../../components/PokemonGenId';
-import {getPokemonNameById} from '../../apis/pokemons';
+import { getPokemonNameById } from '../../apis/pokemons';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {EventResult} from '../../types/events';
-import {results} from '../../constants/results';
+import { EventResult } from '../../types/events';
+import { results } from '../../constants/results';
 import EventBattleTable from './EventBattleTable';
 
 // type Pokemon = {
@@ -51,8 +51,73 @@ const CatchEventBattleHistory: React.FC = () => {
   const [showPokemons, setShowPokemons] = useState(true)
 
   const sortedRes = [...results].reverse();
-  console.log('results', results)
-  console.log('sortedRes', sortedRes)
+
+
+
+  const mainPart = result && (
+    <>
+      <Typography variant="h4" gutterBottom>Event - <span className="text-[#87CEEB]">{result.rules.eventName}</span></Typography>
+      <div className="border rounded p-2 mb-2 mt-2">
+        <div>Rules: <span className="text-[#ffb3b3]">catch as much as possible!</span>
+        </div>
+        <div>From: {result.rules.dateStart} 00:00</div>
+        <div>Upto: {result.rules.dateEnd} 23:59</div>
+        <div>Days: {daysCount}</div>
+      </div>
+      <div>
+        <div className="flex gap-2">
+          {presentedGens.map((gen) => (
+            <div style={{ backgroundColor: getGenColor(gen) }}
+              className="rounded-[6px] p-1 w-fit text-black">Gen {gen}</div>
+          ))}
+        </div>
+      </div>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
+        {needPokemons.map((pok) => (
+          <div key={pok.id}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pok.id}.png`}
+              alt=""
+            />
+
+            {pok.name} <PokemonGenId id={pok.id} />
+          </div>
+        ))}
+      </div>
+      <br />
+      <hr />
+      <div className="flex mb-2 mt-2 gap-4">
+        {!showTable && (
+          <>
+            <Button style={{ margin: 0 }} variant="contained" onClick={() => setShowPokemons(prev => !prev)} sx={{ mt: 2 }}>
+              {!showPokemons ? 'Show' : 'Hide'} Pokemons
+            </Button>
+            <Button style={{ margin: 0 }} variant="outlined" onClick={() => setSortByTime(prev => !prev)} sx={{ mt: 2 }}>
+              Set sort by: {sortByTime ? 'catch time' : 'standard'}
+            </Button>
+          </>
+        )}
+
+        <Button style={{ margin: 0 }} variant="outlined" onClick={() => setShowTable(prev => !prev)} sx={{ mt: 2 }}>
+          View by: {showTable ? 'default' : 'table'}
+        </Button>
+      </div>
+      <hr />
+      <br />
+
+      {/*------------------------ DRAW USERS*/}
+      {!showTable && result.users.length > 0 && (
+        <Box mb={4}>
+          {result.users.map((user, idx) => (
+            <BattlePlayerView sortByTime={sortByTime} key={idx} user={user} pokemonIds={pokemonIds} idx={idx} showPokemons={showPokemons} />
+          ))}
+        </Box>
+      )}
+    </>
+  )
+
+
   return (
     <div>
       <Accordion expanded={expanded} onChange={() => setExpanded(!expanded)}>
@@ -61,80 +126,43 @@ const CatchEventBattleHistory: React.FC = () => {
         </AccordionSummary>
         <AccordionDetails>
           {sortedRes.map((re) => (
-            <div style={{marginBottom: '16px'}}>
+            <div style={{ marginBottom: '16px' }}>
               Event: <span className="text-[#87CEEB]">{re.rules.eventName}</span>
-              <Button style={{margin: '0 16px', padding: 0}} variant="contained" onClick={() => setResult(re)}>
+              <Button style={{ margin: '0 16px', padding: 0 }} variant="contained" onClick={() => setResult(re)}>
                 Load
               </Button>
               <div>
-                
+
                 <span className="text-[#c6e9f7]">started</span>: {re.rules.dateStart} | <span className="text-[#c6e9f7]">days</span>: {differenceInDays(new Date(re.rules.dateEnd), new Date(re.rules.dateStart)) + 1} | <span className="text-[#c6e9f7]">trainers</span>: {re.users.length}
               </div>
-              </div>
+            </div>
           ))}
         </AccordionDetails>
       </Accordion>
 
 
-      {result && (
+      {!showTable && result && (
         <Box>
-          <Typography variant="h4" gutterBottom>Event - <span className="text-[#87CEEB]">{result.rules.eventName}</span></Typography>
-          <div className="border rounded p-2 mb-2 mt-2">
-            <div>Rules: <span className="text-[#ffb3b3]">catch as much as possible!</span>
-            </div>
-            <div>From: {result.rules.dateStart} 00:00</div>
-            <div>Upto: {result.rules.dateEnd} 23:59</div>
-            <div>Days: {daysCount}</div>
-          </div>
-          <div>
-            <div className="flex gap-2">
-              {presentedGens.map((gen) => (
-                <div style={{backgroundColor: getGenColor(gen)}}
-                     className="rounded-[6px] p-1 w-fit text-black">Gen {gen}</div>
-              ))}
-            </div>
-          </div>
-
-          <div style={{display: 'flex', flexWrap: 'wrap', gap: '16px'}}>
-            {needPokemons.map((pok) => (
-              <div key={pok.id}>
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pok.id}.png`}
-                  alt=""
-                />
-
-                {pok.name} <PokemonGenId id={pok.id}/>
-              </div>
-            ))}
-          </div>
-          <br/>
-          <hr/>
-          <div className="flex mb-2 mt-2 gap-4">
-            <Button style={{margin: 0}} variant="contained" onClick={() => setShowPokemons(prev => !prev)} sx={{mt: 2}}>
-              {!showPokemons ? 'Show' : 'Hide'} Pokemons
-            </Button>
-            <Button style={{margin: 0}} variant="outlined" onClick={() => setSortByTime(prev => !prev)} sx={{mt: 2}}>
-              Set sort by: {sortByTime ? 'catch time' : 'standard'}
-            </Button>
-             <Button style={{margin: 0}} variant="outlined" onClick={() => setShowTable(prev => !prev)} sx={{mt: 2}}>
-                      View by: {showTable ? 'default' : 'table'}
-             </Button>
-          </div>
-          <hr/>
-          <br/>
-
-          {/*------------------------ DRAW USERS*/}
-          {!showTable && result.users.length > 0 && (
-            <Box mb={4}>
-              {result.users.map((user, idx) => (
-                <BattlePlayerView sortByTime={sortByTime} key={idx} user={user} pokemonIds={pokemonIds} idx={idx} showPokemons={showPokemons}/>
-              ))}
-            </Box>
-          )}
-
           {showTable && result.users.length > 0 && (
-           <EventBattleTable pokemonIds={pokemonIds} users={result.users}/>
-           )}
+            <EventBattleTable pokemonIds={pokemonIds} users={result.users} />
+          )}
+          {mainPart}
+        </Box>
+      )}
+
+
+      {showTable && result && (
+        <Box>
+          <div className="flex">
+            <div className="flex-1 p-4">
+              {mainPart}
+            </div>
+            <div className="flex-1 p-4">
+              {result.users.length > 0 && (
+                <EventBattleTable pokemonIds={pokemonIds} users={result.users} />
+              )}
+            </div>
+          </div>
         </Box>
       )}
     </div>
